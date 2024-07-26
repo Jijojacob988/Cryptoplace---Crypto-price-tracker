10f9914d964df7d121857ee816e07cf0ef5d2770
Jijo Jacob <Jijojacob988@gmail.com>
2024-07-26 09:41:09 +0530

    made a footer and made home page responsive

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @ 2024, Jijo Jacob - All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
